67f3b1264e5dc5df1f8756308985a9082477d117
Milan Fernando <milanburny@gmail.com>
2024-06-17 16:46:51 +0100

    Added Portfolios from the json into the scroller

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
-
-const scrollers = document.querySelectorAll(".scroller");
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-    addAnimation();
-}
-
-
-function addAnimation(){
+function addAnimation(scrollers){
     scrollers.forEach((scroller) => {
         //plays animation only if the person has reduced motion off 
         scroller.setAttribute("data-animated", true);
@@ -21,3 +14,40 @@ function addAnimation(){
         });
     });
 }
+function scroller(){
+        const scrollers = document.querySelectorAll(".scroller");
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        addAnimation(scrollers);
+    }
+
+}
+
+function portfolio(project){
+    const portfolio = document.querySelector(".portfolio")
+    portfolio.innerHTML += 
+    `
+    <div>
+        <h1>${project.title}</h1>
+        <img src="${project.image}" height=150px width=200px>
+        <p>${project.description}</p>
+        <button class="border bg-gray-500 p-1 info">More Info</button>
+        <a class="ml-24 text-right" href="${project.github}"><i class="fa-brands fa-github"></i></a>
+        <a class="text-right" href="${project.link}"><i class="fa-regular fa-file"></i></a><br>
+        <p class=hidden>${project.contributors}</p>
+        <p class=hidden>${project.dependencies}</p>
+    </div>
+    `
+}
+
+function getJson(){
+    fetch('portfolio.json').then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        data.projects.forEach((project)=>{
+            portfolio(project)
+        })
+        scroller()
+    })
+}
+
+getJson()
