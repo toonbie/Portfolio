b611e24b3a3898cef440e78e9d720fb693e34412
Milan Fernando <milanburny@gmail.com>
2024-06-18 14:31:00 +0100

    Added more interactivity to my portfolio

--- scripts.js
+++ scripts.js
@@ -30,13 +30,27 @@ function portfolio(project){
         <h1>${project.title}</h1>
         <img src="${project.image}" height=150px width=200px>
         <p>${project.description}</p>
-        <button class="border bg-gray-500 p-1 info">More Info</button>
+        <button class="moreInfo border bg-gray-500 p-1 info">More Info</button>
         <a class="ml-24 text-right" href="${project.github}"><i class="fa-brands fa-github"></i></a>
         <a class="text-right" href="${project.link}"><i class="fa-regular fa-file"></i></a><br>
-        <p class=hidden>${project.contributors}</p>
-        <p class=hidden>${project.dependencies}</p>
+        <p class="toggle hidden"> Contributor: ${project.contributors} </p>
+        <p class="toggle hidden"> Dependencies: ${project.dependencies}</p>
     </div>
     `
+    
+
+}
+function setPortfolioButton(){
+    const buttons = document.querySelectorAll(".moreInfo")
+    buttons.forEach((button)=>{
+        button.addEventListener("click",(e)=>{
+            const alertField = button.parentElement.querySelectorAll(".toggle")
+            alertField.forEach((field)=>{
+                field.classList.toggle("hidden")
+            })
+
+        })
+    });
 }
 
 function getJson(){
@@ -45,6 +59,8 @@ function getJson(){
     }).then((data)=>{
         data.projects.forEach((project)=>{
             portfolio(project)
+            setPortfolioButton()
+            
         })
         scroller()
     })
